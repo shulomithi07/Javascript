f8387b33f8c565c03174d884097968c8fc6d22b2
shulomithi07 <62322842+shulomithi07@users.noreply.github.com>
2021-09-12 23:36:29 +0530

    Intro, Variables, namingConventions of variables.

---
+++ introduction/firstProgram.js
@@ -0,0 +1,35 @@
+console.log("Hello World");
+
+
+// Values and Variables
+// var --> variable(Key)
+// variableName --> name of the variable which can be accessed later 
+// 'randomStuff' --> value of the variable
+
+
+var variableName = 'randomStuff';
+console.log(variableName);
+
+
+// Naming convention : Rules and Best Practices
+/*
+
+* The first letter must be a letter or an underscore (_) or a dollar ($).
+* You cannot use number as a first character
+* The characters other than the first character can have any letter, any number or underscore.
+* You cannot use any other characters other than the above mentioned onces including spaces.
+* Variable names are caseSensitive
+* No limit to the lenght of the variable name
+* You cannot use any of the javascripts reserved words as a variable name.
+
+*/
+
+
+// Test
+// Validation
+
+// var _myVariable = "abc"; // Valid
+// var _1my__Variable = "xyz"; //Valid
+// var 1myVariable = "xbz" //Invalid
+// var $myVariable = "AbC" //Valid
+// var $my Variable = "Xyz" //Invalid
